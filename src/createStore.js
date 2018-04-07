a2107365bc9cd496937dc35c0f1c6a77b2ad141b
imamsutono <imamssutono@gmail.com>
2018-04-07 21:03:11 +0700

    Add redux store for apps

---
+++ src/createStore.js
@@ -0,0 +1,19 @@
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import createLogger from 'redux-logger';
+import newsFeedReducer from './reducers/newsFeedReducer';
+import searchTermReducer from './reducers/searchTermReducer';
+
+const logger = createLogger();
+
+export default (initialState = {}) => {
+  // this function will create store with two state properties
+  // then applies the logger middleware
+  createStore(
+    combineReducers({
+      news: newsFeedReducer,
+      searchTerm: searchTermReducer
+    }),
+    initialState,
+    applyMiddleware(logger)
+  );
+}
